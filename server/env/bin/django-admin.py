89900b75ab1a5e3d2e7e46944535d718de401b55
Logan Ray <lray@Logans-MacBook-Pro.local>
2020-01-21 17:32:27 -0800

    Login Registration

---
+++ server/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/lray/Documents/code/bootcamp/python_stack/django/django_full_stack/Login_Reg/server/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
